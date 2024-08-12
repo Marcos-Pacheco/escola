5cb5bbad7e2c5c58e4354c72c563d1d24936ac95
Marcos <marcos.hr.pacheco@gmail.com>
2024-08-12 10:14:31 -0300

    ✨ feat: adicionando middleware de controle de acesso de rota

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => [0 => 'root', 1 => 'administração', 2 => 'docente', 3 => 'discente'][$value]
+        );
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route::prefix('locacao')->name('locacao.')->group(function () {
+//     Route::get('/', [App\Http\Controllers\gfac\VeiculoLocacaoController::class, 'index'])->name('index');
+//     Route::post('/', [App\Http\Controllers\gfac\VeiculoLocacaoController::class, 'store'])->name('store');
+//     Route::delete('/{locacao_id}/delete', [App\Http\Controllers\gfac\VeiculoLocacaoController::class, 'destroy'])->name('delete');
+// });
+
